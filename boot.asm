66c59dceda67261d3ce87a554769b11eca01514f
Jose Enrique Padilla <jp06171@gmail.com>
2024-06-01 19:50:08 -0600

    Enabled universal start for bootloader

--- boot.asm
+++ boot.asm
@@ -1,7 +1,23 @@
-ORG 0x7c00  ; origin of the program
-BITS 16    ; 16-bit mode
+ORG 0       ; origin of the program
+BITS 16     ; 16-bit mode to run on real mode
 
+_start:
+    jmp short start ; jump to the start of the program
+    nop             ; no operation
+
+times 33 db 0       ; fill the first 33 bytes with 0 after short jump
 start:
+    jmp 0x7c0:step2 ; jump to the start of the program
+
+step2:    
+    cli             ; clear interrupts
+    mov ax, 0x07C0  ; set the data segment to 0x07C0
+    mov ds, ax      ; load the data segment
+    mov es, ax      ; load the extra segment
+    mov ax, 0x00    ; set the stack pointer to 0x0000
+    mov ss, ax      ; load the stack segment
+    mov sp, 0x7c00  ; load the stack pointer
+    sti             ; enables interrupts
     mov si, message ; load the address of the message
     call print      ; call the print function
     jmp $           ; infinite loop
