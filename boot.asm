cb61ea3a53339b37819692c562660a07d9b4c497
Jose Enrique Padilla <jp06171@gmail.com>
2024-06-01 23:29:57 -0600

    Load bootloader in protected mode

--- boot.asm
+++ boot.asm
@@ -1,62 +1,73 @@
-ORG 0       ; origin of the program
+ORG 0x7c00       ; origin of the program
 BITS 16     ; 16-bit mode to run on real mode
+
+CODE_SEG equ gdt_code - gdt_start ; calculate the size of the code segment
+DATA_SEG equ gdt_data - gdt_start ; calculate the size of the data segment
 
 _start:
     jmp short start ; jump to the start of the program
     nop             ; no operation
 
 times 33 db 0       ; fill the first 33 bytes with 0 after short jump
+
 start:
-    jmp 0x7c0:step2 ; jump to the start of the program
+    jmp 0:step2 ; jump to the start of the program
 
 step2:    
     cli             ; clear interrupts
-    mov ax, 0x07C0  ; set the data segment to 0x07C0
+    mov ax, 0x00  ; set the data segment to 0x07C0
     mov ds, ax      ; load the data segment
     mov es, ax      ; load the extra segment
-    mov ax, 0x00    ; set the stack pointer to 0x0000
     mov ss, ax      ; load the stack segment
     mov sp, 0x7c00  ; load the stack pointer
     sti             ; enables interrupts
 
-    mov ah, 2       ; read the sector from the disk
-    mov al, 1       ; read one sector
-    mov ch, 0       ; cylinder number
-    mov cl, 2       ; sector number
-    mov dh, 0       ; head number
-    mov bx, buffer  ; load the buffer
-    int 0x13        ; call BIOS disk interrupt
-    jc error        ; jump to error if there is an error
+.load_protected:
+    cli               ; clear interrupts
+    lgdt [gdt_descriptor] ; load the GDT
+    mov eax, cr0      ; load the control register 0
+    or eax, 0x1       ; set the protected mode bit
+    mov cr0, eax      ; load the control register 0
+    jmp CODE_SEG:load32 ; jump to the protected mode
 
-    mov si, buffer  ; load the buffer
-    call print      ; call the print function
+gdt_start:         ; start of the GDT
+gdt_null:          ; null descriptor   
+    dd 0x0         ; set the base to 0
+    dd 0x0         ; set the limit to 0
 
-    jmp $           ; infinite loop
+gdt_code:          ; code descriptor
+    dw 0xffff      ; set the limit to 0xffff 0-15 bits
+    dw 0           ; set the base to 0
+    db 0           ; set the base to 0
+    db 0x9a        ; set the access byte
+    db 11001111b   ; set the flags
+    db 0           ; set the base to 0
 
-error:
-    mov si, error_message ; load the error message
-    call print            ; call the print function
-    jmp $                 ; infinite loop
+gdt_data:          ; data descriptor DS, ES, SS, FS, GS
+    dw 0xffff      ; set the limit to 0xffff 0-15 bits
+    dw 0           ; set the base to 0
+    db 0           ; set the base to 0
+    db 0x92        ; set the access byte
+    db 11001111b   ; set the flags
+    db 0           ; set the base to 0
 
-print:
-    mov bx, 0       ; set the page number to 0
-.loop:              ; loop to print the message
-    lodsb           ; load the next byte from the message
-    cmp al, 0       ; check if it is the null character
-    je .done        ; if it is, we are done
-    call print_char ; call the print_char function
-    jmp .loop       ; repeat the loop
-.done:              ; done printing the message     
-  ret               ; return from the function 
+gdt_end:           ; end of the GDT
 
-print_char:
-    mov ah, 0eh ; teletype output
-    int 0x10    ; call BIOS video interrupt
-    ret         ; return from the function
+gdt_descriptor:    ; GDT descriptor
+    dw gdt_end - gdt_null - 1 ; set the size of the GDT
+    dd gdt_start               ; set the base of the GDT
 
-error_message: db "Failed to load sector", 0
+[BITS 32]
+load32:
+    mov ax, DATA_SEG ; load the data segment
+    mov ds, ax       ; load the data segment
+    mov es, ax       ; load the extra segment
+    mov fs, ax       ; load the stack segment
+    mov gs, ax       ; load the stack pointer
+    mov ss, ax       ; load the stack segment
+    mov ebp, 0x00200000 ; set the base pointer
+    mov esp, ebp        ; set the stack pointer
+    jmp $
 
 times 510-($-$$) db 0   ; fill 510 bytes with 0
 dw 0xaa55               ; boot signature
-
-buffer:
