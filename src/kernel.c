c54353ad9eee3b4f1c4aed3d0cb2416f73ffd4a4
Jose Enrique Padilla <jp06171@gmail.com>
2024-06-08 19:09:13 -0600

    implemented paging for memory

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
 
 uint16_t *video_memory = (uint16_t *)0;
 uint16_t terminal_row = 0;
@@ -82,6 +83,7 @@ void print(const char* str)
   }
 }
 
+static struct paging_4gb_chunk* kernel_chunk = 0;
 void kernel_main()
 {
   terminal_initialize();
@@ -92,6 +94,24 @@ void kernel_main()
 
   // Initialize the IDT
   idt_init();
+
+  // Initialize the paging
+  kernel_chunk = paging_4gb(PAGING_IS_WRITABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+
+  // Switch to the kernel paging
+  paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+  char* ptr = kzalloc(4096);
+  paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_IS_PRESENT | PAGING_IS_WRITABLE | PAGING_ACCESS_FROM_ALL);
+
+  // Enable paging
+  enable_paging();
+
+  char *ptr2 = (char *)0x1000;
+  ptr2[0] = 'A';
+  ptr2[1] = 'B';
+  print(ptr2);
+  print(ptr);
 
   // Enable interrupts
   enable_interrupts();
